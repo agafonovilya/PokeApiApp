35d183bdd7075c84e40916de1e9f6703c9aa18d7
Ilya Agafonov <60569587+agafonovilya@users.noreply.github.com>
2021-08-24 16:20:29 +0300

    Bottom Navigation implemented

--- app/src/main/java/ru/agafonovilya/pokeapiapp/MainActivity.kt
+++ app/src/main/java/ru/agafonovilya/pokeapiapp/MainActivity.kt
@@ -2,17 +2,48 @@ package ru.agafonovilya.pokeapiapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import androidx.fragment.app.Fragment
+import ru.agafonovilya.pokeapiapp.databinding.MainActivityBinding
 import ru.agafonovilya.pokeapiapp.ui.main.MainFragment
 
 class MainActivity : AppCompatActivity() {
 
+    private lateinit var binding: MainActivityBinding
+    private var selectedFragment: Fragment? = null
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.main_activity)
-        if (savedInstanceState == null) {
-            supportFragmentManager.beginTransaction()
-                .replace(R.id.container, MainFragment.newInstance())
-                .commitNow()
+
+        binding = MainActivityBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.bottomNavigation.setOnItemSelectedListener { item ->
+            when(item.itemId) {
+                R.id.search_by_name -> { showToast("search_by_name") }
+                R.id.random_search -> { showToast("random_search") }
+                R.id.favorites -> { showToast("favorites") }
+            }
+            selectedFragment?.let { replaceFragment(it) }
+            true
         }
+
+        if (savedInstanceState == null) {
+            selectedFragment = MainFragment.newInstance()
+            replaceFragment(selectedFragment as MainFragment)
+        }
+
+    }
+
+    private fun replaceFragment(fragment: Fragment){
+        supportFragmentManager
+            .beginTransaction()
+            .replace(R.id.container, fragment)
+            .commit()
+    }
+
+    fun showToast(message: String) {
+        Toast.makeText(this, message, Toast.LENGTH_SHORT).show()
     }
 }
