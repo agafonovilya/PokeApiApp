9eb4d00967c4239a01c12e59d3d44a0ac0e3c524
Ilya Agafonov <60569587+agafonovilya@users.noreply.github.com>
2021-08-26 14:28:22 +0300

    added scroll view and view image

--- app/src/main/java/ru/agafonovilya/pokeapiapp/view/ByNameFragment.kt
+++ app/src/main/java/ru/agafonovilya/pokeapiapp/view/ByNameFragment.kt
@@ -64,6 +64,7 @@ class ByNameFragment : Fragment() {
 
     private fun fillViews(pokemon: Pokemon) {
         binding.byNameFragmentName.text = pokemon.name
+        Injection.provideImageLoader().loadInto(pokemon.sprites.other.officialArtwork.front_default, binding.byNameFragmentImage)
     }
 
     override fun onDestroyView() {
